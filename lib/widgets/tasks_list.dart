5e787f54a121c1a9c83c488925c99018472c7773
Andrej <amilanovi0@gmail.com>
2022-08-30 09:45:45 +0200

    Checking off To-Do list tasks

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo_flutter/models/task.dart';
 import 'package:todo_flutter/models/task_data.dart';
 import 'task_tile.dart';
 import 'package:provider/provider.dart';
@@ -12,13 +11,12 @@ class TaskList extends StatelessWidget {
     return Consumer<TaskData>(builder: (BuildContext context, taskData, child) {
       return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
             return TaskTile(
-                isChecked: taskData.tasks[index].isDone,
-                taskTitle: taskData.tasks[index].name,
+                isChecked: task.isDone,
+                taskTitle: task.name,
                 checkboxCallback: (checkboxState) {
-                  // setState(() {
-                  //   widget.tasks[index].toggleDone();
-                  // });
+                  taskData.updateTask(task);
                 });
           },
           itemCount: taskData.taskCount);
